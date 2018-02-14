d9ff772f114f1da46b8dde2976a255621b227501
Kalla Sai Nookeswara Rao <35985234+Nookesh@users.noreply.github.com>
2018-02-14 20:51:49 +0530

    Add files via upload

---
+++ Client1-MessQ.c
@@ -0,0 +1,50 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <stdio.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct msg_buffer 
+    {
+         long msg_type;
+         char msg_text[100];
+    }message;
+
+    key_t key;
+    int msgid,len;
+
+    /* Geting a unique key value */
+    if((key = ftok("Clint-MessQ.c", '1')) == -1)
+    {
+         printf("\nFtoken Error");
+         return 0;
+    }
+
+    /* Creating a message queue using and giving read write permissions */
+    if(-1 == (msgid = msgget(key, (0644 | IPC_CREAT))))
+    {
+         printf("\nMsgget Error");
+         return 0;
+    }
+   
+    /* This client uses 1 as type */
+    message.msg_type = 1;
+
+    printf("\nEnter the expression you want to compute(+ or - or *)\n eg:a * b ");
+    gets(message.msg_text);
+  
+    len = strlen(message.msg_text) + 1;
+
+    /* Sending input data to server */
+    if (msgsnd(msgid, &message, len, 0) == -1)
+         printf("\nmsgsnd"); 
+
+    /* Receiving result from client of type-1 only */
+    msgrcv(msgid, &message, sizeof(message), 1, 0);
+    printf("\nResult obtained from server is %s",message.msg_text); 
+
+    return 0;
+}
